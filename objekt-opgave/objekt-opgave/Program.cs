5367dd39298709b36ed1fecacde36332ff11a1d8
[Adam] <[adam7305@elev.techcollege.dk]>
2022-03-15 14:30:15 +0100

    Made it Rasmus/Idiot secure

--- objekt-opgave/objekt-opgave/Program.cs
+++ objekt-opgave/objekt-opgave/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 class BankAccount
 {
-    static int balance;
+    private static double balance;
     static void Main(string[] args)
     {
         BankAccount account = new BankAccount();
@@ -11,18 +11,31 @@ class BankAccount
             Console.WriteLine("Vælg en handling:");
             Console.WriteLine("1. Hæv penge");
             Console.WriteLine("2. Indsæt penge");
+            Console.WriteLine("3. Vis balance på konto");
             Console.Write(">");
             string valg = Console.ReadLine();
 
-            switch (int.Parse(valg))
+            if (valg == "" || !int.TryParse(valg, out int number))
             {
-                case 1:
-                    Withdraw();
-                    break;
-                case 2:
-                    Deposit();
-                    break;
-            }
+                Console.WriteLine("Stop being an idiot\n");
+            } else
+            {
+                switch (int.Parse(valg))
+                {
+                    case 1:
+                        Withdraw();
+                        break;
+                    case 2:
+                        Deposit();
+                        break;
+                    case 3:
+                        TotalBalance();
+                        break;
+                    default:
+                        Console.WriteLine("Stop being an idiot\n");
+                        break;
+                }
+            }            
         }
     }
 
@@ -30,22 +43,29 @@ class BankAccount
     {
         Console.WriteLine("Indstast det ønskede beløb, der skal hæves fra kontoen:");
         Console.Write(">");
-        int beloeb = int.Parse(Console.ReadLine());
-        Console.WriteLine("\nPengene blev hævet!\nNy saldo: " + (balance - beloeb + "\n"));
-        balance -= beloeb;
+        double beloeb = double.Parse(Console.ReadLine());
+        if((balance - beloeb) < 0)
+        {
+            Console.WriteLine("Du kan ikke trække over på denne konto!\n");
+        }
+        else
+        {
+            Console.WriteLine("\nPengene blev hævet!\nNy saldo: " + (balance - beloeb + "\n"));
+            balance -= beloeb;
+        }
     }
 
     static void Deposit()
     {
         Console.WriteLine("Indstast det ønskede beløb, der skal indsættes på kontoen:");
         Console.Write(">");
-        int beloeb = int.Parse(Console.ReadLine());
+        double beloeb = double.Parse(Console.ReadLine());
         Console.WriteLine("\nPengene blev overført!\nNy saldo: " + (balance + beloeb + "\n"));
         balance += beloeb;
     }
 
     static void TotalBalance()
     {
-
+        Console.WriteLine("Den nuværende balance på kontoen er: " + balance + "\n");
     }
 }
