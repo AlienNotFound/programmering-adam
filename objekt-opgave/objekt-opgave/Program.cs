d4b3a767d35df1536ad580bec98230b62d6f47c6
[Adam] <[adam7305@elev.techcollege.dk]>
2022-03-15 12:54:49 +0100

    Updated objekt-opgave files

---
+++ objekt-opgave/objekt-opgave/Program.cs
@@ -0,0 +1,33 @@
+﻿class BankAccount
+{
+    int balance;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Vælg en handling:");
+        Console.WriteLine(">");
+        string valg = Console.ReadLine();
+
+        switch (int.Parse(valg))
+        {
+            case 1:
+                Console.WriteLine(valg + " er valgt");
+                break;
+        }
+    }
+
+    static void Withdraw()
+    {
+
+    }
+
+    static void Deposit()
+    {
+
+    }
+
+    static void TotalBalance()
+    {
+
+    }
+}
